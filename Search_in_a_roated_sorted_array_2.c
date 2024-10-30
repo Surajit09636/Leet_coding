3a0b18661e64b50184c385d04e48e1e29cbd3746
my-demo1 <surajitsutradhar010@gmail.com>
2024-10-30 09:33:32 +0530

    Add a new file

---
+++ Search_in_a_roated_sorted_array_2.c
@@ -0,0 +1,31 @@
+bool search(int* nums, int numsSize, int target) {
+    int start = 0, end = numsSize - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
+            return true;
+        }
+
+        if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+            start ++;
+            end --;
+        } else if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+    }
+
+    return false;
+}
